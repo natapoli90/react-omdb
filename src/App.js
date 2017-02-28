1374560ff2d4a481a6989ad8d7132cc22f610404
Natalia Polishchuk <npolishchuk@grabitsystems.com>
2017-02-28 11:49:55 -0800

    render hard-coded results

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>REACT OMDB</h2>
+        <h2><a href="/">OMDB (REACT)</a></h2>
         <SearchContainer />
       </div>
     );
--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -1,33 +1,44 @@
 import React, {Component} from 'react'
 import Search from "./Search"
+import Results from "./Results.js"
 
 class SearchContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      query: ''
+      query: '',
+      hasSearched: false,
     }
   }
   onSearchInput(e) {
     this.setState({
-      query: e.target.value
+      query: e.target.value,
     })
   }
   onsubmitQuery(e) {
     e.preventDefault();
-    console.log(this.state.query);
     this.state = {
-      query: ''
+      query: '',
+      hasSearched: true,
     }
   }
   render() {
-    return (
-      <Search
-      handleSearchInput={(e) => this.onSearchInput(e)}
-      handleSubmitQuery={(e) => this.onSubmitQuery(e)}
-      query={this.state.query}
-      />
-    )
+    let movies = [
+      {title: "Star Wars", poster_url: "http://fallmeeting.agu.org/2015/files/2015/12/Star-Wars.jpg"},
+      {title: "Top Gun", poster_url: "http://ecx.images-amazon.com/images/I/51YimkRDEjL._SY445_.jpg"}
+    ]
+    if(this.state.hasSearched){
+      return(
+        <Results movies={movies} />
+      )
+    } else {
+      return(
+        <Search
+        handleSearchInput={(e) => this.onSearchInput(e)}
+        handleSubmitQuery={(e) => this.onSubmitQuery(e)}
+        query={this.state.query} />
+      )
+    }
   }
 }
 
