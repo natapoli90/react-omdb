0dd1a46ed551ff3ddccd62a0f7a55cfa2cdeeb01
Natalia Polishchuk <npolishchuk@grabitsystems.com>
2017-02-28 10:35:03 -0800

    wire up form with event listener

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 
 class Search extends Component {
+  submitQuery(e){
+    e.preventDefault();
+    console.log("submitted");
+  }
   render() {
     return (
     <div className="serachBox">
-      <form>
+      <form onSubmit={(e) => this.submitQuery(e)}>
         <input type="text" placeholder="Enter a Movie Title" />
         <button type="submit">Search</button>
       </form>
